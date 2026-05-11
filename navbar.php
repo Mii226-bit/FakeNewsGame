6f9c3e435e454b0d650c3156fb7f5507d3fbe016
Harumo2 <gakkou1127@gmail.com>
2026-05-11 12:42:55 +0900

    2026/05/11

---
+++ navbar.php
@@ -0,0 +1,33 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<nav class="navbar navbar-expand-lg custom-navbar">
+  <div class="container-fluid">
+
+    <a class="navbar-brand" href="#">MySite</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">👑ランキング</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">👤プロフィール</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">📒見直し</a>
+        </li>
+
+      </ul>
+    </div>
+
+  </div>
+</nav>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
